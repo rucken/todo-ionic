970fa3908452c55ccaa2862aa59161d475146e70
EndyKaufman <admin@site15.ru>
2019-04-08 10:13:33 +0500

    chore: Add NgxRemoteConfigIonicService used native HTTP client, for correct load config over CORS

---
+++ apps/demo/src/app/utils/ngx-remote-config.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { HTTP } from '@ionic-native/http/ngx';
+import { Platform } from '@ionic/angular';
+import { INgxRemoteConfig, NgxRemoteConfigService, NGX_REMOTE_CONFIG } from 'ngx-remote-config';
+
+@Injectable()
+export class NgxRemoteConfigIonicService<T = any> extends NgxRemoteConfigService {
+    constructor(
+        private http: HTTP,
+        private platform: Platform,
+        @Inject(NGX_REMOTE_CONFIG) private options: INgxRemoteConfig,
+        private httpClient: HttpClient
+    ) {
+        super(
+            options,
+            httpClient
+        );
+    }
+    initConfigAsync() {
+        if (this.platform.is('android') || this.platform.is('ios')) {
+            return new Promise(resolve =>
+                this.http.disableRedirect(false).then(result =>
+                    this.http.get(this.options.url, {}, {}).then(
+                        response => {
+                            let config;
+                            try {
+                                config = JSON.parse(response.data);
+                            } catch (error) {
+                                config = response.data;
+                            }
+                            this.config$.next(config);
+                            resolve(config);
+                        },
+                        response => {
+                            this.config$.next({});
+                            resolve({});
+                        }
+                    )
+                )
+            );
+        }
+        return super.initConfigAsync();
+    }
+}
